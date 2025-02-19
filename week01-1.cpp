6f5f481878c8a1238dfbec0c4e0ad67aa8aacf5c
pjc116 <a0979003552@gmail.com>
2025-02-19 11:55:30 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,15 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-s 存檔成 week01-1.cpp 要記得加 .cpp
+/// 下週考試考這題, 不用藍色的"中文註解"
+#include <stdio.h> /// 檔名不能寫錯
+/// stdio 的意思是 標準的standard input/output
+/// 有了 stdio.h 的外掛
+int main()
+{
+	int a, b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("%d", a+b);
+}
