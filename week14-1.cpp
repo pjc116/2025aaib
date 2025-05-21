3aaf4808213f431923f01a452b4691302a4eec44
pjc116 <a0979003552@gmail.com>
2025-05-21 11:55:25 +0800

    week14

---
+++ week14-1.cpp
@@ -0,0 +1,16 @@
+// week14-1.cpp
+// UVA11063 B2-Sequence
+#include <stdio.h>
+int main()
+{
+	int a[100];
+	int N, t=1;
+	while( scanf("%d", &N) == 1){ // Part 1
+		for(int i=0; i<N; i++){ // Part 1: intup
+			scanf("%d", &a[i]);
+		}
+		// Part 2:output
+		printf("Case #%d: It is a B2-Sequence.\n\n", t);
+		t++;
+	}
+}
